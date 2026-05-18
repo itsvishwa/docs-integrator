e54f78981ed9a55f7a4a23559c1c498a25d3f827
Thevakumar-Luheerathan <Luhee27@gmail.com>
2026-05-18 15:26:34 +0530

    Fix wrong system requirment pages

--- en/sidebars.ts
+++ en/sidebars.ts
@@ -2226,7 +2226,6 @@ const sidebars: SidebarsConfig = {
           type: 'category',
           label: 'Appendix',
           items: [
-            'reference/appendix/system-requirements',
             'reference/error-code',
             'reference/glossary',
             'reference/faq',
